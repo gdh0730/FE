3732bbca19699769ce7221d5b6cf075f70286fd7
joshuayeyo <chiikawayeyo@gmail.com>
2025-05-01 19:34:37 +0900

    Init: First commit!

---
+++ android/app/src/main/kotlin/com/example/heal_lo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.heal_lo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:heal_lo/screens/home/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'HealLo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Home(),
+    );
+  }
+}
