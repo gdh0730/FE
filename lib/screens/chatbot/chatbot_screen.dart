b78647b636c6cd5952fe4b1093e2e20267747c1a
joshuayeyo <chiikawayeyo@gmail.com>
2025-05-15 12:02:49 +0900

    Feat(#15): Add Chatbot Screen

---
+++ lib/screens/chatbot/chatbot_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:heal_lo/widgets/commons/app_bar.dart';
+
+class ChatbotScreen extends StatefulWidget {
+  const ChatbotScreen({super.key});
+
+  @override
+  State<ChatbotScreen> createState() => _ChatbotScreenState();
+}
+
+class _ChatbotScreenState extends State<ChatbotScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CommonAppBarWidget(),
+      body: SafeArea(
+        child: Center(
+          child: const Text("추후 추가될 기능입니다."),
+        ),
+      ),
+    );
+  }
+}
